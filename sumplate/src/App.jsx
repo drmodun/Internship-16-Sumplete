3c40c87d112ee40322abb6a91688505c2f1b4848
= <jan.modun.st@gmail.com>
2023-03-15 15:31:26 +0100

    made columns and added numbrs

--- sumplate/src/App.jsx
+++ sumplate/src/App.jsx
@@ -1,13 +1,26 @@
 import { Row } from "./components/Game/Row";
 import { Square } from "./components/Game/Square";
 import "./index.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [values, setValues] = useState([
+    [1, 1, 1],
+    [1, 2, 3],
+    [1, 5, 7],
+  ]);
+  const [resultRows, setResultRows] = useState([1, 2, 3]);
+  const [resultColumns, setResultColumns] = useState([1, 2, 3]);
   return (
-    <div className="App">
-      <Row result ={1} values = {[1,1,1]}></Row>
+    <div className="column">
+      <Row result ={1} values = {values[0]}></Row>
+      <Row result ={2} values = {values[1]}></Row>
+      <Row result ={3} values = {values[2]}></Row>
+      <div className="result-row">
+        {resultColumns.map((result) => {return <span>{result}</span>})}
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
